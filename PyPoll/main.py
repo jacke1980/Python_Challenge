4781715c934e1c2d716039b71a98c1db9f094c20
jackeline Gonzales B <jackiegb@jackelines-Mini.attlocal.net>
2022-03-25 18:19:09 -0400

    fixed python code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,112 +1,83 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[33]:
-
-
-import os
+#import packages 
 import csv
-import pandas as pd
+import os
+
+#define function to fix percentage format to 3 decimal points 
+def fixPercent(num):
+    num = "{:.3%}".format(num)
+    return num
+
+input_file = os.path.join("Resources", "election_data.csv")
+output_file = os.path.join("Analysis", "pyPoll_analysis.txt")
 
 
-# In[34]:
+#create empty lists and variables for storing data #
+Candidates = []
+VoteCounts = []
+VotePercent = []
+TotalVotes = 0
+WinnerCount = 0
 
+#read the CSV file 
+with open(input_file, 'r') as election_data:
+    reader = csv.reader(election_data, delimiter=",")
 
-#Reference to Poll csv file
-Poll_Path = os.path.join("Resources", "election_data.csv")
-Poll_Path_df = pd.read_csv(Poll_Path, encoding="utf-8")
-Poll_Path_df.head()
+    Headers = next(reader)
 
+#for loop to go through each row in the CSV file and count the total number of votes 
 
-# In[35]:
+    for row in reader:
+        TotalVotes += 1
+    
+#get candidate names and individual vote counts and store in lists
+        if row[2] not in Candidates:
 
+#append the name to Candidates and a value of 1 to VoteCounts list
+            Candidates.append(row[2])
+            VoteCounts.append(1)
 
-#Total Votes
-Votes = len(Poll_Path_df)
+#if row[2] (candidate name) is in the Candidates list
+        else:
 
+# get the index of the candidate from the Candidates list in order to increase the vote count by 1
+            CandidateIndex = Candidates.index(row[2])
+            VoteCounts[CandidateIndex] += 1
+        
 
+# calculate percentage of votes for each candidate 
+for i in range(len(VoteCounts)):
+    VotePercent.append(VoteCounts[i] / TotalVotes)
 
-# In[36]:
+# calculate the winner based on most votes 
+for i in range(len(VoteCounts)):
 
+# if the number of votes is greater than WinnerCount (initially zero)
+    if VoteCounts[i] > WinnerCount:
+        
+#update WinnerCount to the number of votes at index i
+        WinnerCount = VoteCounts[i]
 
-#Finding candidates
-Candidates=list(Poll_Path_df["Candidate"].unique())
+#update Winner to the candidate name at index i
+        Winner = Candidates[i]
 
+#create a text file with the analysis output 
+with open(output_file, 'w') as textfile:
+    textfile.write(f"Election Results\n"
+                   f"----------------------------\n"
+                   f"Total Votes: {TotalVotes}\n"
+                   f"----------------------------\n"
+                   )
 
+# for loop to iteratively write each candidate's info
+    for i in range(len(Candidates)):
+        textfile.write(f"{Candidates[i]}: {fixPercent(VotePercent[i])} ({VoteCounts[i]})\n")
 
-# In[37]:
+    textfile.write(f"----------------------------\n"
+                   f"Winner: {Winner}\n"
+                   f"----------------------------\n"
+                  )
 
-
-#Finding votes by candidate
-Vote_chart=Poll_Path_df["Candidate"].value_counts()
-
-
-
-# In[38]:
-
-
-Vote_chart_df=pd.DataFrame(Vote_chart)
-Vote_chart_df["percent"]=round((Vote_chart_df["Candidate"]/Votes*100),2)
-Vote_chart_df
-
-
-# In[39]:
-
-
-Vote_chart_df.dtypes
-
-
-
-# In[40]:
-
-
-Vote_chart_df["percent"]= "%"+ Vote_chart_df["percent"].astype(str) 
-
-
-# In[41]:
-
-
-Vote_chart_df=Vote_chart_df.rename(columns={"Candidate":"Votes", "percent":"Percent"})
-chart_df=pd.DataFrame(Vote_chart_df)
-chart_df
-
-
-# In[42]:
-
-
-#Finding the Winner
-Winner="Khan"
-Winner
-
-
-# In[44]:
-
-
-print("-------------------------------")
-print(" ELECTIONS RESULTS")
-print("-------------------------------")
-print(" Total votes: "+ str(Votes))
-print("-------------------------------")
-print( f'{chart_df}')
-print("-------------------------------")
-print(" Winner:   "+   str(Winner))
-print("-------------------------------")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+#read the output file and print analysis to terminal 
+with open (output_file, 'r') as analysis:
+    results = analysis.read()
+    print(results)
